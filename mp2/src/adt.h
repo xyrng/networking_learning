4cee68011082feeb0bf77a8441bb492777a4aaeb
Zhengdai Hu (David) <zh6@illinois.edu>
2019-10-12 23:10:03 -0500

    adts

---
+++ mp2/src/adt.h
@@ -0,0 +1,32 @@
+/*
+ * Author: nonsense
+ *
+ * Created on Oct 6, 2019
+ */
+#pragma once
+
+#include <stdio.h>
+#include <sys/types.h>
+#include "constants.h"
+
+typedef struct statistics {
+    size_t cwnd = 1;
+    size_t estRtt = 20;
+    size_t devRtt = 0;
+    size_t totalBytes;
+    size_t sentBytes;
+    size_t sendBase;
+    size_t nextSeq;
+} tStat;
+
+typedef struct queue {
+    char* buffer[WINDOW] = {0};
+    int epollfd = 0;
+    size_t start = 0;
+    size_t end = 0;
+    size_t length = 0;
+} senderQ;
+
+void buffer(queue* q, char* buf);
+void restartTimer(queue* q, size_t pos);
+void remove(queue* q, size_t pos);
---
+++ mp2/src/constants.h
@@ -0,0 +1,11 @@
+/*
+ * Author: nonsense
+ *
+ * Created on Oct 6, 2019
+ */
+#pragma once
+
+#define PACKET_LENGTH 1024
+#define WINDOW 4096
+
+#define 
