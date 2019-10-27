7729d9945a14b55a94b84b82c4c1896e4f11d7dc
xyrng <xyr1217@gmail.com>
2019-10-27 00:51:06 -0500

    mp2 receiver draft

--- mp2/src/adt.h
+++ mp2/src/adt.h
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-/*
- * Author: nonsense
- *
- * Created on Oct 6, 2019
- */
-#pragma once
-
-#include <stdio.h>
-#include <sys/types.h>
-#include "constants.h"
-
-typedef struct statistics {
-    size_t cwnd = 1;
-    size_t estRtt = 20;
-    size_t devRtt = 0;
-    size_t totalBytes;
-    size_t sentBytes;
-    size_t sendBase;
-    size_t nextSeq;
-} tStat;
-
-typedef struct squeue {
-    char* buffer[WINDOW] = {0};
-    int epollfd = 0;
-    size_t start = 0;
-    size_t end = 0;
-    size_t length = 0;
-} senderQ;
-
-typedef struct rqueue {
-    char* buffer[WINDOW] = {0};
-    size_t start = 0;
-    size_t end = 0;
-    size_t length = 0;
-} receiverQ;
-
-typedef struct rdt_packet {
-    uint16_t  src_port;
-    uint16_t  dst_port;
-    uint32_t  seq_num;
-    uint32_t  ack_num;
-    uint16_t  payload;         // each time need atoi() to convert
-    char      fin_byte;
-    char*     data;
-} rdt_packet;
-
-void buffer(queue* q, char* buf);
-void restartTimer(queue* q, size_t pos);
-void remove(queue* q, size_t pos);
-||||||| merged common ancestors
-=======
 /*
  * Author: nonsense
  *
@@ -137,4 +85,19 @@ typedef struct ack_packet {
     uint32_t ack_num;
     char     fin_byte;
 } ack_packet;
->>>>>>> shared
+
+typedef struct rdt_packet {
+    uint32_t  seq_num;
+    uint32_t  ack_num;
+    uint16_t  payload;              //data size
+    char      fin_byte;
+    char      data[1024];
+} rdt_packet;
+
+typedef struct rqueue {
+    rdt_packet    buffer[WINDOW];
+    size_t        buflen[WINDOW];      // length of each packet
+    size_t        start = 0;
+    size_t        end = 0;
+    size_t        length = 0;          // queue length
+} receiverQ;
--- mp2/src/receiver_main.c
+++ mp2/src/receiver_main.c
@@ -1,12 +1,6 @@
 /*
  * File:   receiver_main.c
-<<<<<<< HEAD
- * Author:
-||||||| merged common ancestors
- * Author: 
-=======
  * Author: nonsense
->>>>>>> shared
  *
  * Created on Oct 6, 2019
  */
@@ -15,19 +9,12 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <pthread.h>
-<<<<<<< HEAD
-#include <adt.h>
-
-||||||| merged common ancestors
-
-=======
+#include "adt.h"
 #include "constants.h"
->>>>>>> shared
 
 
 struct sockaddr_in si_me, si_other;
@@ -38,19 +25,96 @@ void diep(char *s) {
     exit(1);
 }
 
-// data should be on the heap
-void parse_packet(char* buff, rdt_packet* packet, ssize_t size) {
-
+// TODO: check stack mem problem
+void build_ack_packet(ack_packet *ack_packet, rdt_packet *packet, uint32_t new_ack_num) {
+    packet->seq_num = packet->ack_num;
+    ack_packet->ack_num = new_ack_num;
+    ack_packet->fin_byte = packet->fin_byte;
 }
 
+int write_to_file(int fd, rdt_packet *packet) {
+    ssize_t writed_size = 0;
+    ssize_t total_size = (ssize_t) packet->payload;
+    do {
+        ssize_t round = write(fd, packet->data + writed_size, total_size - writed_size);
+        if (round == -1) {
+            perror("Error");
+            exit(1);
+        }
+        writed_size += round;
+    } while (writed_size < total_size);
+    return 0;
+}
+
+void check_and_sent_buffer(int write_fd, int socket_fd, uint32_t *seq_num, receiverQ *rec_queue) {
+    // maybe bug: uint32_t or int
+    uint32_t ret_seq_num = *seq_num;
+    // maybe bug: pointer++
+    rdt_packet *temp_pkt = rec_queue->buffer + (ret_seq_num % WINDOW);
+    ack_packet *ack_pkt;
+    while (temp_pkt != NULL && *temp_pkt != NULL) {
+        // write that packet to the file
+        if (write_to_file(write_fd, temp_pkt) != 0) {
+            fprintf(stderr, "Write to file Failure.\n");
+            exit(1);
+        }
+        ret_seq_num++;
+        build_ack_packet(ack_pkt, temp_pkt, ret_seq_num);
+        send_ack_packet(socket_fd, ack_pkt);
+        memset(temp_pkt, 0, sizeof(rdt_packet));
+        temp_pkt = rec_queue->buffer + ret_seq_num;
+    }
+    *seq_num = ret_seq_num;
+}
+
+void send_ack_packet(int socket_fd, ack_packet *ack_pkt) {
+    if (sendto(socket_fd, ack_pkt, sizeof(ack_packet), 0,
+               (struct sockaddr *)&si_other, sizeof(si_other)) == -1) {
+        if (errno == EINTR) {
+            continue;
+        }
+        diep("[Error]send_ack_packet");
+    }
+}
+
+void map_to_queue(receiverQ *queue, rdt_packet *packet) {
+    uint32_t seq_num = packet->seq_num % WINDOW;
+    memset(queue->buffer + seq_num, packet, sizeof(rdt_packet));
+}
+
+// TODO: check
+int recvPacket(int sockfd, rdt_packet* const packet) {
+    struct sockaddr_storage addr_container = {0};
+    socklen_t addr_len = sizeof(addr_container);
+    struct sockaddr* from_addr = &addr_container;
+
+    size_t pkt_len = sizeof(rdt_packet);
+    size_t numbytes;
+    while (numbytes = recvfrom(sockfd, packet, pkt_len, 0, from_addr, addr_len) == -1) {
+        if (errno == EINTR) {
+            continue;
+        }
+        diep("recvfrom");
+    }
+    if (numbytes != pkt_len) {
+        warn("Packet Reading Failer [Size]");
+        return -1;
+    }
+    if (packet->seq_num == 0) {
+        si_other.sin_family = AF_INET;
+        si_other.sin_port = from_addr->sin_port;
+        si_other.sin_addr.s_addr = from_addr->sin_addr.s_addr;
+    }
+    // TODO: check
+    if (from_addr->sin_port != si_other.sin_port || from_addr->sin_addr.s_addr != si_other.sin_addr.s_addr) {
+        warn("Ignoring packets from nonrelevant addr");
+        return -1;
+    }
+    return 0;
+}
 
 void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
-<<<<<<< HEAD
 
-||||||| merged common ancestors
-    
-=======
->>>>>>> shared
     slen = sizeof (si_other);
 
 
@@ -66,45 +130,54 @@ void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
         diep("bind");
 
 
-<<<<<<< HEAD
 	/* Now receive data and send acknowledgements */
-    // receive from socket
+    receiverQ rec_queue = {0};
+    uint32_t last_ack_num = 0;
+    int write_file_fd;
+    if ((write_file_fd = open(destinationFile, O_WRONLY | O_APPEND | O_CREAT)) != -1) {
+        do {
+            rdt_packet *packet;
+            ack_packet *ack_pkt;
+            if (recvPacket(s, packet) != 0){
+                fprintf(stderr, "Parsing Packet Failure.\n");
+                if (last_ack_num == 0) {
+                    continue;
+                }
+                // send dup ack
+                build_ack_packet(ack_pkt, packet, last_ack_num);
+                send_ack_packet(s, ack_pkt);
+                continue;
+            }
+            // write to file or save to queue
+            if (packet->seq_num == last_ack_num) {
+                if (write_to_file(fd, packet) != 0) {
+                    // TODO: check this situation handling.
+                    fprintf(stderr, "Write to file Failure.\n");
+                    exit(1);
+                } else {
+                    last_ack_num = packet->seq_num + 1;
+                    build_ack_packet(ack_pkt, packet, last_ack_num);
+                    send_ack_packet(s, ack_pkt);
+                    check_and_sent_buffer(write_file_fd, s, &last_ack_num, &rec_queue);
+                }
+            } else if (packet->seq_num > last_ack_num) {
+                map_to_queue(&rec_queue, packet);
+                build_ack_packet(ack_pkt, packet, last_ack_num);
+                send_ack_packet(s, ack_pkt);
+            } else {
+                build_ack_packet(ack_pkt, packet, last_ack_num);
+                send_ack_packet(s, ack_pkt);
+            }
 
-    char buffer[4096];
-    uint32_t ack_num = 0;
-    uint32_t last_payload = 0;
-    FILE *fd = fopen(destinationFile, 'a');
-    do {
-        ssize_t size = read(s, &buffer, 4096);
-        rdt_packet *packet;
-        parse_packet(&buffer, packet, size);
-        // get seq num
-        if (packet->seq_num == last_seq_num + last_payload) {
-            // append write to file
-            do () {
-                fwrite();
-            } while();
-            // send ack_num
-            last_payload = (uint32_t) packet->payload;
-            ack_num = packet->seq_num + last_payload;
-
-        } else {
-            // mem_map to buffer
-            // send dupc_ack_num
-        }
-    } while(1)
-
-    fclose(fp);
-||||||| merged common ancestors
-	/* Now receive data and send acknowledgements */    
-
-=======
-    /* Now receive data and send acknowledgements */
-    while (1) {
-
+            if (packet->fin_byte == '1') {
+                break;
+            }
+        } while(1)
+    } else {
+        perror("Error");
     }
 
->>>>>>> shared
+    close(fp);
     close(s);
     printf("%s received.", destinationFile);
     return;
