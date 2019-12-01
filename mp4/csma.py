7d8e77130aa8e0b2226c7c688715a2a9ca4ebb10
Zhengdai Hu (David) <zh6@illinois.edu>
2019-12-01 15:56:56 -0600

    Debug optimized csma

--- mp4/csma.py
+++ mp4/csma.py
@@ -1,15 +1,12 @@
-import sys
 from random import randint
 from statistics import pvariance
-from functools import total_ordering
-from heapq import heapify, heapreplace
+from heapq import heapify, heapreplace, heappop, heappush
 
 
-@total_ordering
 class Node:
-    def __init__(self, id, R, M):
-        self.id = id
-        self._reset_R(R)
+    def __init__(self, idx, R, M):
+        self.id = idx
+        self._reset_r(R)
         self.coll_ceil = M
         self.num_colls = 0
         self.num_trans = 0
@@ -19,18 +16,6 @@ class Node:
     def __lt__(self, other):
         return (self.count_down_val, self.id) < (other.count_down_val, other.id)
 
-    def __le__(self, other):
-        return (self.count_down_val, self.id) <= (other.count_down_val, other.id)
-
-    def __ge__(self, other):
-        return (self.count_down_val, self.id) >= (other.count_down_val, other.id)
-
-    def __gt__(self, other):
-        return (self.count_down_val, self.id) > (other.count_down_val, other.id)
-
-    def __eq__(self, other):
-        return (self.count_down_val, self.id) == (other.count_down_val, other.id)
-
     @property
     def r_val(self):
         return self.r_list[self._r_id]
@@ -39,26 +24,26 @@ class Node:
     def has_timeout(self):
         return self.count_down_val < 1
 
-    def _reset_R(self, R):
+    def _reset_r(self, R):
         if R is not None:
             self.r_list = R
         self._r_id = 0
 
-    def _permute_R(self):
+    def _permute_r(self):
         self._r_id = 0 if self._r_id + 1 > self.coll_ceil else self._r_id + 1
 
     def _rand_back_off(self):
         self.count_down_val = randint(0, self.r_val)
 
     def reset(self, R=None):
-        self._reset_R(R)
+        self._reset_r(R)
         self.num_colls = 0
         self.num_trans = 0
         self._rand_back_off()
 
-    def meet_collision(self):
+    def collide(self):
         self.num_colls += 1
-        self._permute_R()
+        self._permute_r()
         self._rand_back_off()
 
     def sent_packet(self):
@@ -67,7 +52,8 @@ class Node:
 
 
 class CSMA:
-    def __init__(self, config):
+    def __init__(self, config, name='Default Model'):
+        self.name = name
         self.num_nodes = config['N']
         self.packet_len = config['L']
         self.r_list = config['R']
@@ -91,13 +77,18 @@ class CSMA:
 
         for node in self.nodes:
             node.reset(R)
-        for i in range(self.num_nodes, N):
-            self.nodes.append(Node(i, R=self.r_list, M=self.coll_ceil))
+        if N is not None:
+            for i in range(self.num_nodes, N):
+                self.nodes.append(Node(i, R=self.r_list, M=self.coll_ceil))
+            self.num_nodes = N
         heapify(self.nodes)
-        self.num_nodes = N
         self.sending_time = 0
         self.idle_time = 0
         self.coll_time = 0
+
+    @property
+    def min_candidate(self):
+        return self.nodes[0]
 
     """
       0: counting down
@@ -105,36 +96,48 @@ class CSMA:
     """
 
     def simulate(self, N=None, L=None, R=None):
-        if N != None or L != None or R != None:
+        if N is not None or L is not None or R is not None:
             self._reinitialize(N=N, L=L, R=R)
         time = 0
         occupied_flag = False
+        next_idle_time = 0
         while time < self.tol_time:
             if occupied_flag:
                 occupied_flag = False
                 self.sending_time += self.packet_len
-                self.nodes[0].sent_packet()
-                heapreplace(self.nodes, self.nodes[0])
-
-            min_candidate = self.nodes[0]
-            if not min_candidate.has_timeout:
-                idle = min_candidate.count_down_val
-                time += idle
-                self.idle_time += idle
+                self.min_candidate.sent_packet()
+                heapreplace(self.nodes, self.min_candidate)
+            elif next_idle_time > 0:
+                self.idle_time += next_idle_time
                 for node in self.nodes:
-                    node.count_down_val -= idle
-            elif self.nodes[1].has_timeout or self.nodes[2].has_timeout:  # collision
+                    node.count_down_val -= next_idle_time
+
+            next_idle_time = self.min_candidate.count_down_val
+            if next_idle_time > 0:
+                time += next_idle_time
+            elif self.nodes[1].has_timeout or self.nodes[2].has_timeout:
                 time += 1
                 self.coll_time += 1
-                while self.nodes[0].has_timeout:
-                    self.nodes[0].meet_collision()
-                    heapreplace(self.nodes, self.nodes[0])
+                timeout_again = []
+                while self.min_candidate.has_timeout:
+                    self.min_candidate.collide()
+                    if self.min_candidate.has_timeout:
+                        timeout_again.append(heappop(self.nodes))
+                    else:
+                        heapreplace(self.nodes, self.min_candidate)
+                for node in timeout_again:
+                    heappush(self.nodes, node)
             else:
                 time += self.packet_len
                 occupied_flag = True
 
-        if occupied_flag:  # Simulation terminates before tranmission finished
-            self.sending_time += self.packet_len - (time - self.tol_time)
+        # Simulation terminates before transmission/count down finishes
+        if occupied_flag:
+            self.sending_time += self.tol_time - (time - self.packet_len)
+        elif next_idle_time > 0:
+            self.idle_time += self.tol_time - (time - next_idle_time)
+
+        return self
 
     def get_util(self):
         return self.sending_time / self.tol_time
@@ -152,7 +155,6 @@ class CSMA:
         return sum(node.num_colls for node in self.nodes)
 
     def create_report(self, file_name):
-        assert self.coll_time + self.idle_time + self.sending_time == self.tol_time
         print("self.sending_time: %d" % self.sending_time)
         print("self.idle_time: %d" % self.idle_time)
         total_collisions = self.get_total_collisions()
@@ -172,6 +174,8 @@ class CSMA:
             file.write(
                 "Variance in number of collisions (across all nodes): %f\n" % self.var_coll())
 
+        return self
+
 
 def read_config(fname):
     with open(fname) as f:
@@ -180,11 +184,11 @@ def read_config(fname):
 
 
 if __name__ == "__main__":
+    import sys
+
     if len(sys.argv) != 2:
         print('./csma input.txt')
         exit()
     else:
         config = read_config(sys.argv[1])
-    test = CSMA(config)
-    test.simulate()
-    test.create_report("output.txt")
+        CSMA(config).simulate().create_report("output.txt")
